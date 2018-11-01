3eb590e8cbfac04ec8415f6402747ad53bdf1579
Mathis Delaunay <mathisdelaunay@MacBook-Pro-de-Mathis.local>
2018-11-01 23:18:52 +0100

    Encryption test and setup test

--- src/main/java/model/VCFTest.java
+++ src/main/java/model/VCFTest.java
@@ -1,18 +1,25 @@
 package model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.ColumnTransformer;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
+@Data
 @Table(name="VCFTest")
 @AllArgsConstructor
 public class VCFTest {
     @Id
     @GeneratedValue
     private Integer id;
-    private String test;
+    // SELECT AES_DECRYPT(AES_ENCRYPT('mytext', 'mykeystring'),'mykeystring');
+    @ColumnTransformer(read="AES_DECRYPT(encryptedField,'salut')" ,write="AES_ENCRYPT(?,'salut')")
+    @Column(columnDefinition = "blob")
+    private String encryptedField;
+
+    public VCFTest(String encryptedField) {
+        this.encryptedField = encryptedField;
+    }
 }
--- src/test/java/VCFReaderTest.java
+++ src/test/java/VCFReaderTest.java
@@ -3,6 +3,7 @@ import model.VCFTest;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -11,6 +12,8 @@ import org.junit.Test;
 import utils.VCFReader;
 
 import java.net.URISyntaxException;
+import java.util.Iterator;
+import java.util.List;
 
 public class VCFReaderTest {
 
@@ -19,42 +22,38 @@ public class VCFReaderTest {
     @Test
     public void premierTest() {
         VCFReader reader = VCFReader.getSharedInstance();
-        VCFFile vcfFile;
+        VCFFile vcfFile = null;
         try {
             vcfFile = reader.loadVCFFile("vcfFile.vcf");
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-        Assert.assertEquals(1,1);
+        Assert.assertNotNull(vcfFile);
+        Assert.assertTrue(vcfFile.getBody().size() != 0);
     }
 
-    protected void setUp() throws Exception {
-        // A SessionFactory is set up once for an application!
+    @Test
+    public void testHibernate() {
+        Session session = null;
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure() // configures settings from hibernate.cfg.xml
                 .build();
         try {
             sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-        }
-        catch(HibernateException exception){
-            System.out.println("Problem creating session factory");
-            exception.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testHibernate() {
-        try {
-            setUp();
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.save( new VCFTest("Mathis"));
+            session.getTransaction().commit();
+            VCFTest vcfTest = (VCFTest) session.createQuery("FROM VCFTest").getSingleResult();
+            Assert.assertTrue("Mathis".equals(vcfTest.getEncryptedField()));
+        } catch (HibernateException e) {
+            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (session != null)
+                session.close();
         }
-        /*Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save( new VCFTest(1,"Mathis"));
-        session.getTransaction().commit();
-        session.close();
-        Assert.assertEquals(1,1);*/
     }
 
 }
